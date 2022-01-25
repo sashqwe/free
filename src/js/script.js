944da2053d82188b8b464e2dd17f352af74c46cf
sashqwe <87759522+sashqwe@users.noreply.github.com>
2022-01-25 12:02:58 +0200

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+ $('form').submit(function(e){
+     e.preventDefault();
+     $.ajax({
+        type:"POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+     }).done(function(){
+        $(this).find("input").val("");
+
+        $('form').trigger('reset');
+     });
+     return false;
+ });
+});
